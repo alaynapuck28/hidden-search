011944d79461bea5ac468e11c1ddd6e254a609aa
alaynapuck28 <you@example.com>
2021-01-31 10:51:07 -0600

    widget fun

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+//firtst attempt
+// const container = document.querySelector(".search");
+// const button = document.querySelector(".button");
+// const input = document.querySelector(".input");
 
+// button.addEventListener("click", () => {
+//   console.log(container);
+//   if (container.classList.contains("active")) {
+//     container.classList.remove("active");
+//   } else {
+//     container.classList.add("active");
+//     input.focus();
+//   }
+// });
+
+//better
+const search = document.querySelector(".search");
+const btn = document.querySelector(".button");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
